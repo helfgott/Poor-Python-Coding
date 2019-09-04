4f7ec17f525b805640030cddf8a403a5f1332a92
miguelleonardo.ortiz@bbva.com <miguelleonardo.ortiz@bbva.com>
2019-09-04 13:00:40 -0300

    Organizamos el script mugroso

--- Conectivity.py
+++ Conectivity.py
@@ -10,20 +10,16 @@ Elegir VPNSERVERA o VPNSERVERB si:
 	c) !FIXME: Puede validar una consulta logica a LDAP
 	Si se cumplen a,b,c entonces elige el primer servidor de la lista, en este caso VPNSERVERA
 	Si falla alguno de a,b,c entonces prueba con el segundo servidor de la lista, en este caso VPNSERVERB
-        Finalmente si probamos que funciona todo generar un daemon para monitorizar la llegada y registrar las continuas caidas de red,
-        de este modo obtener estadisticas y clavarlas en le rostro de los que proveen la conectividad.
-
 """
-
 #--------------------------------------------------------------------------------------- DEFINIR SERVIDOR DE INGRESO
 #											 Y AUTENTICACION 
 
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 
-
-VPNSERVERS = ['VPNSERVERB', 'VPNSERVERB']
+VPNSERVERS = ['VPNSERVERA', 'VPNSERVERB']
 invalidServers=[]
 validServer=[]
+DEVNULL = open(os.devnull,'w')
 
 def whichVPNServer():
 	for vpnserver in VPNSERVERS:
@@ -31,29 +27,26 @@ def whichVPNServer():
 		ncLDAP = "nc -vz " + vpnserver + " 389 -w 5"
 		reachVPNSRV = subprocess.Popen(ncSSH.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		outputSRV, errorSRV = reachVPNSRV.communicate()
-		print reachVPNSRV.returncode	
-
-		if not reachVPNSRV.returncode == 0 :
-			# Verificar si se llega al puerto de SSH
-			logging.warning('Proyecto: DNS - no puedo resolver el hostname: ' + vpnserver)
-			invalidServers.append( (vpnserver,outputSRV,errorSRV,'No se pudo llegar al host al puerto 22') )
-		else :
-			# Verififcar si se llega al puerto de LDAP
-			reachVPNLDAP = subprocess.Popen(ncSSH.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		# Si llegamos al 22 del servidor con codigo 0 se avanza
+		if reachVPNSRV.returncode == 0 :
+			reachVPNLDAP = subprocess.Popen(ncSSH.split(), stdout=DEVNULL, stderr=subprocess.STDOUT)
 			outputLDAP, errorLDAP = reachVPNLDAP.communicate()
-			invalidServers.append( (vpnserver,outputLDAP,errorLDAP, 'No se pudo llegar al host al puerto 389')  )
-			# valida si llega a 22 y 389 de un servidor, de ser asi lo usa
-			# NTH: validacion logica de LDAP
+			# Si llegamos al 389 del servidor con codigo 0 se avanza
 			if reachVPNLDAP.returncode == 0 :
 				logging.debug('Proyecto: Llego al puerto 22 en: ' + vpnserver)
 				logging.debug('Proyecto: Llego al puerto 389 en: ' + vpnserver)
 			        validServer.append( (vpnserver, '22', '389') )	
 			else :
-				invalidServers.append( (vpnserver,outputLDAP,'No llego a LDAP') )
-		
+				# Si no llegamos al 389 del servidor lo ponemos en la lista negra
+				invalidServers.append( (vpnserver,outputLDAP,errorLDAP, 'No se pudo llegar al host al puerto 389')  )
+		else :	
+			# si no llegamos al 22 del servidor lo ponemos en la lista negra
+			logging.warning('Proyecto: DNS - no puedo resolver el hostname: ' + vpnserver)
+			invalidServers.append( (vpnserver,outputSRV,errorSRV,'No se pudo llegar al host al puerto 22') )
+
 selectedServers = whichVPNServer()
 
 print "invalidos:", invalidServers
-print "validos:", validServer 
+print "validos:", validServer
 
-
+ 

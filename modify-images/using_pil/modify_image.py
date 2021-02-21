115bbeaa437d75f7be88458195c1f277a637a8dd
Miguel Ortiz (helfgott) <contacto@migueleonardortiz.com.ar>
2021-02-21 12:59:22 -0500

    Final project week 1

--- modify-images/using_pil/modify_image.py
+++ modify-images/using_pil/modify_image.py
@@ -4,6 +4,23 @@
 #                 pip3 install pillow
 
 from PIL import Image
+import os, os.path
 
-im = Image.open('myimage.jpg')
-im.rotate(45).save('myimage_rotates.jpg')
+
+imgPath='/home/student-01-5c85541b1d5e/images/'
+imgSavePath='/opt/icons/'
+
+print ('Original Images')
+for f in os.listdir(imgPath):
+    if not f.startswith('.'):
+        img = Image.open(imgPath + f)
+        print(img.format, img.size)
+        img = img.convert('RGB')
+        img = img.resize((128, 128))
+        img.rotate(90).save(imgSavePath + f + '.jpeg')
+
+print ('Fixed Images')
+for f in os.listdir(imgSavePath):
+    img = Image.open(imgSavePath + f)
+    print(img.format, img.size)
+
